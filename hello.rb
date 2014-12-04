705381e442655a4486bcd172e14618ebd7845128
byelipk <byelipk@gmail.com>
2014-12-04 09:57:20 -0500

    add instructions for how to squash commits
    make more changes
    
    make even more changes
    
    add instructions

--- hello.rb
+++ hello.rb
@@ -1,3 +1,11 @@
 puts "Hello world"
 puts "How are you?"
 puts "Goodnight, and GOODLUCK!"
+
+puts "I think I got this"
+puts "First you make some changes"
+puts "Then you keep making changes till you are done"
+puts "Be sure to commit each discrete change"
+puts "Then use `git rebase -i HEAD~<number of commits to roll into one>`"
+puts "Squash the commits you want to squash."
+puts "Then force push to the remote repo with `git push -f`"
